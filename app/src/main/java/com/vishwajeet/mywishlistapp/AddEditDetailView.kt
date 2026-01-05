aa0a55d6324733d41f2a701e436a667b0eab70fa
Vishwajeet <vishwajeett42@gmail.com>
2026-01-05 13:07:56 +0530

    commit successfully added and loded a wish

--- app/src/main/java/com/vishwajeet/mywishlistapp/AddEditDetailView.kt
+++ app/src/main/java/com/vishwajeet/mywishlistapp/AddEditDetailView.kt
@@ -88,7 +88,11 @@ fun AddEditDetailView(
                 if (viewModel.wishTitleState.isNotEmpty() &&
                     viewModel.wishDescriptionState.isNotEmpty()){
                        if (id != 0L){
-
+                           viewModel.updateWish(Wish(
+                               id = id,
+                               title = viewModel.wishTitleState.trim(),
+                               description =  viewModel.wishDescriptionState.trim()
+                           ))
 
                        }else{
                               viewModel.addWish(wish =
--- app/src/main/java/com/vishwajeet/mywishlistapp/HomeView.kt
+++ app/src/main/java/com/vishwajeet/mywishlistapp/HomeView.kt
@@ -1,6 +1,9 @@
 package com.vishwajeet.mywishlistapp
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,15 +12,23 @@ import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
+import androidx.compose.material.DismissDirection
+import androidx.compose.material.DismissValue
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.FractionalThreshold
 import androidx.compose.material.Icon
+import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.rememberDismissState
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -27,6 +38,7 @@ import com.vishwajeet.mywishlistapp.data.DummyWish
 import com.vishwajeet.mywishlistapp.data.Wish
 import java.nio.file.WatchEvent
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun HomeView(
     navController: NavController,
@@ -55,15 +67,56 @@ fun HomeView(
         LazyColumn (
             modifier = Modifier.fillMaxSize().padding(it)
         ){
-            items(wishList.value){
-                WishItem(it,{
-                         val id = it.id
-                    navController.navigate(Screen.AddScreen.route +"/$id")
-                })
+            items(wishList.value, key = {it.id}){wish ->
+
+                val dismissState = rememberDismissState(
+                    confirmStateChange = {
+                        if (it == DismissValue.DismissedToEnd ||
+                            it == DismissValue.DismissedToStart){
+                            viewModel.deleteWish(wish)
+
+                        }
+                        true
+                    }
+                )
+
+                SwipeToDismiss(
+                    state =  dismissState,
+                    background = {
+                        val color by animateColorAsState(
+                            if (dismissState.dismissDirection
+                                == DismissDirection.EndToStart) Color.Red else Color.Transparent,
+                            label = ""
+                        )
+                        DeleteBackground(color)},
+                    directions = setOf( DismissDirection.EndToStart),
+                    dismissThresholds = { FractionalThreshold(0.75f)},
+                    dismissContent = {
+                        WishItem(wish,{
+                            val id = wish.id
+                            navController.navigate(Screen.AddScreen.route +"/$id")
+                        })
+                    }
+                )
+
+
             }
         }
     }
 
+}
+
+@Composable
+fun DeleteBackground(color: Color) {
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize().padding(horizontal = 2.dp)
+            .background(color),
+        contentAlignment = Alignment.CenterEnd
+    ) {
+      Icon(imageVector = Icons.Default.Delete,contentDescription = null, modifier = Modifier.padding(end = 3.dp))
+    }
 }
 
 @Composable
